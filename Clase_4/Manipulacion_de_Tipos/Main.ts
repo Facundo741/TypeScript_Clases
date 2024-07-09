818387f593cc462a4cedb985f85867e6767d22ce
Facundo <santillanfacundo43@gmail.com>
2024-07-09 09:22:37 -0300

    Completando Type Manipulation

--- Clase_4/Manipulacion_de_Tipos/Main.ts
+++ Clase_4/Manipulacion_de_Tipos/Main.ts
@@ -184,3 +184,241 @@
 // // type de B = string
 // const x: B = 12;
 // const y: B = "hola"; // <- los arreglos no permiten índices tipo string
+
+
+// Type Manipulation, operador typeof
+// JavaScript tiene ya incluido el operador typeof.
+// console.log(typeof "Hola Mundo"); // -> string
+// TypeScript agrega al operador typeof mayor contexto para asignar un tipo a una variable o propiedad.
+// let s = "hola mundo";
+// let n: typeof s; // <- ahora n es tipo string
+// Esto no es muy útil para tipos básicos, pero combinado con otros operadores, se puede utilizar typeof para expresar distintos patrones. Por ejemplo, el tipo ReturnType utiliza internamente typeof para asignar el tipo que retorna una función.
+// type P = (c: unknown) => boolean;
+// type K = ReturnType<P>; // <- el tipo de K es boolean
+// Si intentamos utilizar ReturnType usando el nombre de una función el compilador generará un error.
+// function H() {
+//     return "hola mundo";
+// }
+// type F = ReturnType<H>; // <- esto no esta permitido
+// Limitaciones
+// TypeScript limita intencionalmente los tipos de expresiones que se pueden utilizar con typeof.
+// Específicamente, solo se permite utilizar typeof en identificadores (por ejemplo nombres de variables) en sus propiedades. Esto evite confusiones al momento de escribir el código.
+
+
+// Type Manipulation, IAT, Indexed Access Types (tipos basados en índices)
+// Podemos utilizar indexed access types (tipos basados en índices) para buscar una propiedad específica en otro tipo.
+// type Persona = { edad: number; nombre: string; vivo: boolean };
+// type Edad = Persona["edad"];
+// const edad1: Edad = 5;
+// const edad2: Edad = "quince"; // <- esta línea generará un error
+// Los IAT son en si mismos tipos, por lo que podemos utilizar unions, keyof u otros tipos.
+// type Persona = { edad: number; nombre: string; vivo: boolean };
+// type I1 = Persona["edad" | "nombre"]; // <- union dentro de una referencia a índices
+// type I2 = keyof Persona;
+// let miI2: I2; // <- puede ser number | string o boolean
+// Otro ejemplo de IAT es el poder utilizar number de forma arbitraria para obtener el tipo de los elementos de un arreglo. Esta posibilidad se puede combinar con typeof para convenientemente capturar el tipo de elemento.
+// const miArreglo = [
+//     { nombre: "Martha", edad: 22 },
+//     { nombre: "Zhenya", edad: 31 },
+//     { nombre: "Luis", edad: 28 },
+// ];
+// type Persona = (typeof miArreglo)[number]; // <- combinaciones posibles { nombre: string, edad: number }
+// type OtraPersona = (typeof miArreglo)[number]["nombre"]; // <- string
+// Si el arreglo no es homogéneo, es decir que las propiedades son diferentes para cada uno de los elementos que lo componen, el resultado de typeof miArreglo[number] es la unión de todos las opciones posibles.
+// const miArreglo2 = [
+//     { nombre: "Ana", sexo: "F" },
+//     { nombre: "Luisa", viva: true },
+//     { nombre: "Daria", edad: 40 },
+// ];
+// type Persona2 = (typeof miArreglo2)[number];
+// // <- combinaciones { nombre: string, sexo: string } | { nombre: string, viva: boolean } | { nombre: string, edad: number }
+// Solo se pueden utilizar types cuando se hace indexing, esto quiere decir que no se puede usar const para hacer una referencia a una variable.
+// const key = "edad";
+// type Edad = Persona[key]; // <- esto generará un error
+// Sin embargo, se puede utilizar un alias type.
+// type key = "edad";
+// type Edad = Persona[key];
+
+
+// Type Manipulation, Conditional Types (tipos basados en condicionales)
+// En los programas se hace necesario tomar decisiones basadas en los valores de entrada. En JavaScript esto también es así, y considerando que los valores pueden ser fácilmente determinados, dichas decisiones también están basadas en los valores de entrada.
+// Los tipos condicionales describen la relación entre los valores de entrada y los de salida.
+// interface Animal {
+//     live(): void;
+// }
+// interface Perro extends Animal {
+//     ladrar(): void;
+// }
+// type E1 = Perro extends Animal ? number : string;
+// // E1 = number porque Perro extiende de animal
+// type E2 = RegExp extends Animal ? number : string;
+// //  E2 = string porque RegExp no extiende de Animal
+// Los tipos condicionales toman una forma que luce como expresiones condicionales con el operador ternario (condition ? expresionVerdadera : expresionFalsa).
+// condicion ? tipoSiEsVerdadero : tipoSiEsFalso;
+// Supongamos que deseamos una impresora de etiquetas, esta soporta dos tipos de etiquetas: las etiquetas que imprimen un identificador numérico y las que usan una combinación de números y letras.
+// interface IdEtiqueta {
+//     id: number;
+// }
+// interface NombreEtiqueta {
+//     nombre: string;
+// }
+// function crearEtiqueta(arg: number): IdEtiqueta;
+// function crearEtiqueta(arg: string): NombreEtiqueta;
+// function crearEtiqueta(arg: string | number): IdEtiqueta | NombreEtiqueta;
+// function crearEtiqueta(arg: string | number): IdEtiqueta | NombreEtiqueta {
+//     throw "sin implementar";
+// }
+// crearEtiqueta("randomId");
+// crearEtiqueta(10000477);
+// Hemos utilizado sobrecarga para crear una función que reciba un valor arg como número o string, y en base a ello podemos abstraer la lógica necesaria para desarrollar cada uno de los casos.
+// Otra forma de solucionar este mismo problema es utilizando conditional types.
+// type Arg<T extends string | number> = T extends number
+//     ? IdEtiqueta
+//     : NombreEtiqueta;
+// function crearEtiquetaSimplificado<T extends string | number>(arg: T): Arg<T> {
+//     throw "sin implementar aun";
+// }
+// crearEtiquetaSimplificado("randomId");
+// crearEtiquetaSimplificado(10000477);
+
+
+// Type Manipulation, Conditional Type Constraints (restricciones)
+// Frecuentemente los checks en un type condicional nos proporcionan cierta información adicional. De la misma manera que al usar restricciones como las unions combinados con generics, el uso de tipos condicionales nos permiten también definir restricciones.
+// type MensajeDe<T> = T["mensaje"];
+// // esto generará un error debido a que no sabemos si el tipo tiene una propiedad mensaje
+// Podemos cambiar nuestro indexed type de la siguiente forma para prevenir este error.
+// type MensajeDe<T extends { mensaje: unknown }> = T["mensaje"];
+// ¿Qué sucede si deseamos que MensajeDe pueda tomar cualquier valor, pero que never sea el valor por default cuando la propiedad no exista?
+// Podemos solucionar esto utilizando conditional types.
+// type MensajeDe<T> = T extends { mensaje: unknown } ? T["mensaje"] : never;
+// type Mensaje1 = MensajeDe<{ mensaje: string }>;
+// // type = string
+// type Mensaje2 = MensajeDe<{ mensaje: number }>;
+// // type = number
+// type Mensaje3 = MensajeDe<{}>;
+// // type = never
+// Cuando el resultado de evaluar el condicional es positivo, este nos arroja un tipo dependiendo de la propiedad mensaje que siempre existirá.
+// Obtener el tipo a partir de un arreglo
+// En el siguiente ejemplo, creamos un tipo llamado Flatten que usa el tipo homogéneo almacenado en un arreglo o un tipo por default.
+// type Flatten<T> = T extends any[] ? T[number] : T;
+// type T1 = Flatten<number[]>;
+// // type number
+// type T2 = Flatten<number>;
+// // type = number
+//  Cuando Flatten es utilizado, utiliza un indexed type cuando la parte verdadera se cumple, es decir cuando estamos utilizando un arreglo, de lo contrario hace referencia al tipo utilizado directamente.
+
+
+// Type Manipulation, Inferencia en Conditional Types
+// Los tipos condicionales nos proporcionan una forma de inferir los tipos con los que realizamos comparaciones, estos se obtienen en la parte verdadera utilizando la palabra infer. Para los ejemplos anterior pudimos simplificar mas aun, utilizando infer en lugar de obtener el tipo mediante un indexed type.
+// type Flatten<T> = T extends Array<infer I> ? I : T;
+// type T1 = Flatten<number[]>;
+// // type = number
+// type T2 = Flatten<number>;
+// // type = number
+// Esto también puede ser escrito mediante la sintaxis compacta de los arreglos [infer I];
+// type Flatten2<T> = T extends [infer I] ? I : T;
+// type T3 = Flatten2<string[]>;
+// // type = string
+// type T4 = Flatten2<string>;
+// // type = string
+// En ambos ejemplos hemos utilizado la palabra infer para introducir un nuevo generic llamado I. Esto nos simplifica la obtención del tipo que el arreglo almacena.
+// type ObtenerSalida<T> = T extends () => infer Return ? Return : never;
+// type T1 = ObtenerSalida<() => string>;
+// // type = string
+// type T2 = ObtenerSalida<() => () => number>;
+// // type () => number
+
+
+// Type Manipulation, Distributive Conditional Types (tipos condicionales distributivos)
+// Cuando los tipos condicionales actúan sobre generics se convierten en distributives (distributivos) si se les proporciona una union. Por ejemplo tomemos el siguiente ejemplo.
+// type ConvertirEnArreglo<T> = T extends any ? T[] : never;
+// type T1 = ConvertirEnArreglo<string | number>;
+// // type = string[] | number[]
+// Por lo general el efecto distributivo es el esperado, sin embargo si se desea prevenir este comportamiento podemos encerrar cada elemento a ambos lados de la expresión extends.
+// type ConvertirEnArreglo2<T> = [T] extends [any] ? T[] : never;
+// type T2 = ConvertirEnArreglo2<string | number>;
+// // type = (string | number)[]
+
+
+// Type Manipulation, Mapped Types (tipos basados en mapas)
+// Un mapped type (tipo basado en mapas) es un tipo generic que utiliza una unión generada a partir de keyof para iterar a través de las propiedades de un tipo y crear así otro.
+// type Persona = {
+//     trabaja: boolean;
+//     estudia: boolean;
+//     edad: number;
+// };
+// type Opciones<T> = {
+//     [key in keyof T]: boolean;
+// };
+// const o: Opciones<Persona> = {
+//     estudia: true,
+//     trabaja: true,
+//     edad: 200, // <- esta línea genera un error ya que todos deben ser boolean
+// };
+//  En este ejemplo Opciones tomará el nombre de todas las propiedades de T y cambiará los valores a boolean.
+
+
+// Type Manipulation, Mapped Types, Modifiers (modificadores en mapas basados en tipos)
+// Existen dos modificadores adicionales que pueden ser aplicados durante el proceso de mapeo: readonly y ? que afectan la mutabilidad y opcionalidad de las propiedades.
+// Se pueden agregar o eliminar estos modificadores utilizando el prefijo - o +. Si no se agrega el prefijo, entonces se asume +.
+// type TipoMutable<T> = {
+//     -readonly [P in keyof T]: T[P];
+// };
+// type CuentaBloqueada = {
+//     readonly id: string;
+//     readonly nombre: string;
+// };
+// type CuentaDesbloqueada = TipoMutable<CuentaBloqueada>;
+// const usuario1: CuentaBloqueada = {
+//     id: "0x00001",
+//     nombre: "Nora",
+// };
+// usuario1.nombre = "Daniela"; // <- esta linea generará un error ya que es solo lectura
+// const usuario2: CuentaDesbloqueada = {
+//     id: "0x00001",
+//     nombre: "Abel",
+// };
+// usuario2.nombre = "Tomas"; // <- si podemos reasignar valores
+
+
+// Type Manipulation, Mapped Types, Key Remapping (re-mapeado de propiedades)
+// En TypeScript 4.1 y versiones mas recientes, es posible remapear las propiedades en tipos utilizando la clausula as dentro de un mapped type.
+// type Etiqueta = {
+//     id: number;
+//     cantidad: number;
+// };
+// type Remapear<T> = {
+//     [K in keyof T as `mi${Capitalize<string & K>}`]: T[K];
+// };
+// type Remapeado = Remapear<Etiqueta>;
+// const miEtiqueta: Remapeado = {
+//     miCantidad: 100,
+//     miId: 12122,
+// };
+
+
+// Type Manipulation, TLT : Template Literal Types (tipos literales de plantilla)
+// Los TLT (template literal types o tipos literales de plantilla) están basados en los SLT (string literal types o tipos literales de cadena), y tienen la capacidad de expandirse en muchas cadenas mediante las uniones.
+// Tienen la misma sintaxis que las TLS (template literal string, texto ${expresion}) en JavaScript, pero se utilizan en situaciones de tipos.
+// type Locacion = "mundo";
+// type Saludo = `Hola ${Locacion}`;
+// // type = "Hola mundo"
+// Cuando se utiliza una unión, el tipo es el conjunto de todas las literales de cadena posibles que conforman cada miembro de la unión.
+// type Correo = "correo_mensaje" | "correo_cabeceras";
+// type Destinatario = "destinatario_nombre" | "destinatario_direccion";
+// type UnionMedianteLiteral = `${Correo | Destinatario}_id`;
+// // type = "destinatario_nombre_id" | "destinatario_direccion_id" | "correo_mensaje_id" | "correo_cabeceras_id"
+//  Tener cuidado de utilizar la concatenación, en este caso se concatenó con _id, de otra forma solo producirá la unión Correo | Destinatario.
+
+
+// Type Manipulation, Intrinsic String Manipulation Types (tipos intrínsecos de manipulación de strings)
+// Para ayudar con la manipulación de strings, TS incluye una serie de tipos que pueden ser utilizados para manipular strings. Estos tipos vienen provistos por el compilador para cuestiones de performance.
+// Para convertir los tipos basados en strings en mayúsculas/minúsculas.
+// type Saludo = "Hola Mundo";
+// type SaludoEnMayusculas = Uppercase<Saludo>; // type = "HOLA MUNDO"
+// type SaludoEnMinusculas = Lowercase<Saludo>; // type = "hola mundo"
+// Existen otras opciones como Capitalize<string> y Uncapitalize<string> que crean tipos con el primer caracter en maúscula o minúscula según sea el caso.
+// type SaludoCapitalizado = Capitalize<Saludo>; // type = "Hola Mundo"
+// type SaludoDescapitalizado = Uncapitalize<Saludo>; // type = "hola mundo"
+
+
