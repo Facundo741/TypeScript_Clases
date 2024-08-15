2d8b9978458ecd6baf97561b40dddeb170e1c05a
Facundo <santillanfacundo43@gmail.com>
2024-08-15 09:43:11 -0300

    Class 1 - Data Types

--- Clase_1/Manejo_de_Errores/Main.ts
+++
@@ -1,6 +0,0 @@
-//Manejo de errores, no es posible enviar un array a la funcion cuando el argumento se definio como string.
-// function saludar(nombre: string) {
-//     return "Hola, " + nombre;
-// }
-// console.log(saludar(["Facundo"]));
-
